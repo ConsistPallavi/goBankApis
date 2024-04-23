cbf2a50dbfdef42262ae3f2f04eac39a9277e28e
Pallavi Kumari <pallavi@hylobiz.com>
2024-04-23 19:02:18 +0530

    initial-commit

---
+++ api.go
@@ -0,0 +1,54 @@
+// create api server or handlers
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+// convert apifunc into httphandler func
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			//handle err here
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/account", s.handleAccount)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTranfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ConsistPallavi/goBank
+
+go 1.21.3
+
+require github.com/gorilla/mux v1.8.1
